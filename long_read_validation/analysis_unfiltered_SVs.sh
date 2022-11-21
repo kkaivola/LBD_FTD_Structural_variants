374a59fcb9f86d0edf3f57be316e42b1d7dccd16
kkaivola <91177606+kkaivola@users.noreply.github.com>
2022-11-21 16:44:48 -0500

    Update analysis_unfiltered_SVs.sh

--- long_read_validation/analysis_unfiltered_SVs.sh
+++ long_read_validation/analysis_unfiltered_SVs.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+
+########LBD
+########
+
 #Extract each LBD sample of interest separately from 1% GATK-SV (all variants), include only SVs with an alternative allele (=exclude 0/0s) 
 
 SR_SAMPLES="RES01827 RES01897 RES02284 UMARY-5077 RES08227 UMARY-1571 UMARY-634 RES01859 RES01877 UMARY-1544 RES00032 RES00064 RES00275 UMARY-4542 RES00071 RES00048"
 
 for SAMPLE in $SR_SAMPLES; do
     bcftools view \
+    -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22 \
     --threads $SLURM_CPUS_PER_TASK \
     -s $SAMPLE \
     -Ou \
@@ -151,6 +156,159 @@ done
 ##Run SR DUPs as INS
 i=0
 for SAMPLE in $SR_SAMPLES; do
+    array=($SAMPLES_NANOPORE)
+    for svtype in DUP; do
+        truvari bench \
+        --refdist=200 \
+        --pctsize=0.70 \
+        --pctsim=0 \
+        --sizemax=999999999 \
+        -b vcfs_sniffles2/split_vcfs/${array[${i}]}_INS_fastq_pass.wf_sv.vcf.gz \
+        -c SR_vcfs/split_SR_vcfs/${SAMPLE}_DUP_as_INS_1percfdr_pass_multialleic_alt_SVs.vcf.gz \
+        -f /data/kaivolakk/Homo_sapiens_assembly38/Homo_sapiens_assembly38.fasta \
+        -o truvari_bench/truvari_bench_${SAMPLE}_${svtype} 
+    done
+    i=${i}+1
+done
+
+conda deactivate
+
+
+
+
+########FTD
+########
+
+
+#Extract each FTD sample of interest separately from 1% FDR GATK-SV data, include only SVs with an alternative allele (=exclude 0/0s) 
+FTD_SAMPLES="RES05717 RES05720 RES05722 RES05724"
+
+for SAMPLE in $FTD_SAMPLES; do
+    bcftools view \
+    -r chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22 \
+    -s $SAMPLE \
+    -Ou \
+    /data/ALS_50k/DementiaSeq_SV.LBD.FTD/CLEAN.SV.fromJinHui/FTD_gatksv_1perc_fdr_cleaned_filters_qual_recalibrated/FTD_1perc_fdr.cleaned_filters_qual_recalibrated.LNGsampleID.vcf.gz \
+    | \
+    bcftools view -i 'GT[*]="alt"' \
+    -Oz \
+    -o ${SAMPLE}_altgtonly_FTD_1perc_fdr.cleaned_filters_qual_recalibrated.LNGsampleID.vcf.gz 
+done
+
+#Split by SV type into separate vcfs
+FTD_SAMPLES="RES05717 RES05720 RES05722 RES05724"
+
+for SAMPLE in $FTD_SAMPLES; do
+    for svtype in "DEL" "INS" "DUP" "INV"; do 
+        filt="SVTYPE=\"$svtype\""
+        bcftools view -i $filt \
+        -O z \
+        -o split_SR_vcfs/${SAMPLE}_${svtype}_1percfdr_pass_multialleic_alt_SVs.vcf.gz \
+        ${SAMPLE}_altgtonly_FTD_1perc_fdr.cleaned_filters_qual_recalibrated.LNGsampleID.vcf.gz 
+    done
+done
+
+#Rename DUP SVtype to INS since that is what cuteSV for Nanopore (mostly) uses. But keep DUP file separate from INS to be able to differ DUP from INS
+FTD_SAMPLES="RES05717 RES05720 RES05722 RES05724"
+
+for SAMPLE in $FTD_SAMPLES; do
+    zcat ${SAMPLE}_DUP_1percfdr_pass_multialleic_alt_SVs.vcf.gz | \
+    sed 's/SVTYPE=DUP/SVTYPE=INS/g' > ${SAMPLE}_DUP_as_INS_1percfdr_pass_multialleic_alt_SVs.vcf
+    bgzip ${SAMPLE}_DUP_as_INS_1percfdr_pass_multialleic_alt_SVs.vcf
+done
+
+
+#To match Nanopore format, for INS SR vcfs, make %END = %POS
+##Extract needed info
+FTD_SAMPLES="RES05717 RES05720 RES05722 RES05724"
+
+for SAMPLE in $FTD_SAMPLES; do
+    bcftools query -f '%CHROM\t%POS\t%END\t%POS\n' ${SAMPLE}_INS_1percfdr_pass_multialleic_alt_SVs.vcf.gz >  ${SAMPLE}_end_annotation.txt
+    bgzip -f ${SAMPLE}_end_annotation.txt
+    tabix -f -s1 -b2 -e3 ${SAMPLE}_end_annotation.txt.gz
+done
+
+##Update END flag for INS: but make it STOP named since END cannot be directly altered, then rename to END.
+
+echo "##INFO=<ID=STOP,Number=1,Type=Integer,Description=""End position of the structural variant"">"  > tmp_stop_header.txt
+
+##Add STOP flag and remove END flag
+FTD_SAMPLES="RES05717 RES05720 RES05722 RES05724"
+
+for SAMPLE in $FTD_SAMPLES; do
+    bcftools annotate -a ${SAMPLE}_end_annotation.txt.gz -c CHROM,FROM,TO,STOP -x INFO/END -h tmp_stop_header.txt -Oz -o ${SAMPLE}_tmp.vcf.gz ${SAMPLE}_INS_1percfdr_pass_multialleic_alt_SVs.vcf.gz
+    zcat ${SAMPLE}_tmp.vcf.gz | sed 's/STOP/END/g' > ${SAMPLE}_INS_end_is_pos_1percfdr_pass_multialleic_alt_SVs.vcf
+    bgzip ${SAMPLE}_INS_end_is_pos_1percfdr_pass_multialleic_alt_SVs.vcf
+done
+
+#Index vcfs 
+ls -1 *vcf.gz | parallel -j $SLURM_CPUS_PER_TASK tabix -f -p vcf {}
+
+#Split FTD Nanopore vcfs by SV type
+SAMPLES_NANOPORE="X5324 X4980 X4948 X4904"
+
+for SAMPLE in $SAMPLES_NANOPORE; do
+    for svtype in "DEL" "INS" "DUP" "INV"; do 
+        filt="SVTYPE=\"$svtype\""
+        bcftools view -i $filt \
+        --threads $SLURM_CPUS_PER_TASK \
+        -O z \
+        -o split_vcfs/${SAMPLE}_${svtype}_fastq_pass.wf_sv.vcf.gz \
+        ${SAMPLE}_fastq_pass.wf_sv.vcf.gz 
+    done
+done
+
+
+#Index split Nanopore vcfs by SV type
+parallel -j1 tabix -f -p vcf {} ::: *vcf.gz
+
+#Run truvari bench per SV type per sample
+##Validation criteria are: refdist:200bp, pctsize=0.70, pctsim=0,typeignore=FALSE
+conda activate truvari
+###Nanopore samples in same order than short read samples
+FTD_SAMPLES="RES05717 RES05720 RES05722 RES05724"
+SAMPLES_NANOPORE="X5324 X4980 X4948 X4904"
+
+##DEL,INV
+i=0
+for SAMPLE in $FTD_SAMPLES; do
+    array=($SAMPLES_NANOPORE)
+    for svtype in DEL INV; do
+        truvari bench \
+        --refdist=200 \
+        --pctsize=0.70 \
+        --pctsim=0 \
+        --sizemax=999999999 \
+        -b vcfs_sniffles2/split_vcfs/${array[${i}]}_${svtype}_fastq_pass.wf_sv.vcf.gz \
+        -c SR_vcfs/split_SR_vcfs/${SAMPLE}_${svtype}_1percfdr_pass_multialleic_alt_SVs.vcf.gz \
+        -f /data/kaivolakk/Homo_sapiens_assembly38/Homo_sapiens_assembly38.fasta \
+        -o truvari_bench/truvari_bench_${SAMPLE}_${svtype} 
+    done
+    i=${i}+1
+done
+
+##INS, separately since SR path name is different
+i=0
+for SAMPLE in $FTD_SAMPLES; do
+    array=($SAMPLES_NANOPORE)
+    for svtype in INS; do
+        truvari bench \
+        --refdist=200 \
+        --pctsize=0.70 \
+        --pctsim=0 \
+        --sizemax=999999999 \
+        -b vcfs_sniffles2/split_vcfs/${array[${i}]}_${svtype}_fastq_pass.wf_sv.vcf.gz \
+        -c SR_vcfs/split_SR_vcfs/${SAMPLE}_INS_end_is_pos_1percfdr_pass_multialleic_alt_SVs.vcf.gz \
+        -f /data/kaivolakk/Homo_sapiens_assembly38/Homo_sapiens_assembly38.fasta \
+        -o truvari_bench/truvari_bench_${SAMPLE}_${svtype} 
+    done
+    i=${i}+1
+done
+
+
+##Run SR DUPs as INS
+i=0
+for SAMPLE in $FTD_SAMPLES; do
     array=($SAMPLES_NANOPORE)
     for svtype in DUP; do
         truvari bench \
